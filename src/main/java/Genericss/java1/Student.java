5a4dbdbe3725c6d8d7dde871f8042e3326b27bd7
zzz <867562646@qq.com>
2022-03-23 21:28:44 +0800

    update

---
+++ src/main/java/Genericss/java1/Student.java
@@ -0,0 +1,8 @@
+package Genericss.java1;
+
+/**
+ * @author shkstart
+ * @create 2019 上午 11:58
+ */
+public class Student {
+}
---
+++ src/main/java/Genericss/java2/Student.java
@@ -0,0 +1,8 @@
+package Genericss.java2;
+
+/**
+ * @author shkstart
+ * @create 2019 下午 2:42
+ */
+public class Student extends Person {
+}
